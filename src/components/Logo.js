27659a5ebfa963410501f090e0f887bfb184ca91
Sh.Dilmaghani <shayan.dilmaghani@gmail.com>
2023-07-23 09:50:55 +0330

    finished responsive

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import Link from "next/link";
-import {motion} from "framer-motion";
-
-
-const MotionLink = motion(Link)
-
 const Logo = () => {
     return (
         <div className={'flex items-center justify-center mt-2'}>
