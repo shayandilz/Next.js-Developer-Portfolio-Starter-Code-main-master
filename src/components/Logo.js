8ad91f9cef70d5a979200f2647cefa56f719e0ed
Sh.Dilmaghani <shayan.dilmaghani@gmail.com>
2023-07-23 09:49:09 +0330

    finished responsive

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -8,14 +8,10 @@ const MotionLink = motion(Link)
 const Logo = () => {
     return (
         <div className={'flex items-center justify-center mt-2'}>
-            <MotionLink
-                className={'w-16 h-16 border border-solid border-transparent dark:border-light flex justify-center items-center text-light rounded-full text-2xl font-bold'}
-                whileHover={{
-                    backgroundColor:["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"],
-                    transition: {duration: 1, repeat:Infinity}
-                }}
+            <Link
+                className={'w-16 h-16 border border-solid border-transparent dark:border-light flex justify-center items-center text-light rounded-full text-2xl font-bold bg-dark dark:bg-light dark:text-dark'}
                 href={'/'}>Dilz
-            </MotionLink>
+            </Link>
         </div>
     );
 }
